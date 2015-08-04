715df507605c8c9576da67bd7accbb0014057c39
carlomanf <carlomanf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-04 02:58:28 +0000

    v1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/better-scheduled-posts/trunk@1212223 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ better-scheduled-posts.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Plugin Name: Better Scheduled Posts
+ * Description: Improves the management of your scheduled Posts by making them visible on the front end to administrators/contributors, adding them to the internal link box and enabling you to push them back by any number of days.
+ * Author: Carlo Manf
+ * Author URI: http://carlomanf.id.au
+ * Version: 1.0.0
+ */
+
+// Show scheduled posts to administrators/contributors
+add_filter( 'pre_get_posts', function( $query ) {
+
+	if ( current_user_can( 'edit_posts' ) && !$query->is_singular() && !is_admin() )
+		$query->set( 'post_status', array( 'publish', 'future' ) );
+
+	return $query;
+} );
+
+function bsp_validate_post_data() {
+	$valid = true;
+	$date = explode( '-', $_POST[ 'start_date' ] );
+
+	if ( 3 !== count( $date ) || !checkdate( $date[ 1 ], $date[ 2 ], $date[ 0 ] ) ) {
+		echo '<div class="error"><p>Invalid start date.</p></div>';
+		$valid = false;
+	}
+
+	if ( !intval( $_POST[ 'no_of_days' ] ) ) {
+		echo '<div class="error"><p>Invalid number of days.</p></div>';
+		$valid = false;
+	}
+
+	return $valid;
+}
+
+function bsp_push_posts() {
+
+	$scheduled_posts = get_posts( array( 'post_status' => 'future', 'posts_per_page' => -1 ) );
+	$success = 0;
+
+	foreach ( $scheduled_posts as $post ) {
+		$date = strtotime( $post->post_date );
+		$date_gmt = strtotime( $post->post_date_gmt );
+		$start_date = strtotime( $_POST[ 'start_date' ] );
+
+		if ( $date_gmt < $start_date )
+			continue;
+
+		$updated_post[ 'ID' ] = $post->ID;
+		$updated_post[ 'post_date' ] = date( 'Y-m-d H:i:s', $date + ( (int) $_POST[ 'no_of_days' ] * 86400 ) );
+		$updated_post[ 'post_date_gmt' ] = date( 'Y-m-d H:i:s', $date_gmt + ( (int) $_POST[ 'no_of_days' ] * 86400 ) );
+
+		wp_update_post( $updated_post );
+
+		$success++;
+	}
+
+	if ( $success )
+		echo '<div class="updated"><p>Successfully rescheduled ' . $success . ' post(s)!</p></div>';
+	else
+		echo '<div class="updated"><p>No changes were made because no scheduled posts were found.</p></div>';
+
+}
+
+function bsp_tools_page() {
+
+	if ( !empty( $_POST[ 'push' ] ) )
+		if ( bsp_validate_post_data() )
+			bsp_push_posts();
+
+	?><div class="wrap">
+		<h2>Better Scheduled Posts</h2>
+		<p>Use this tool to push back your scheduled posts.</p>
+		<form method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
+			<table class="form-table">
+				<tr>
+					<th scope="row"><label for="start_date">Start Date</label></th>
+					<td>
+						<input type="text" id="start_date" name="start_date" value=""><p class="description">Format: yyyy-mm-dd</p>
+					</td>
+				</tr>
+				<tr>
+					<th scope="row"><label for="no_of_days">Number of Days</label></th>
+					<td><input type="text" id="no_of_days" name="no_of_days" value=""></td>
+				</tr>
+			</table>
+			<p class="submit"><input type="submit" name="push" class="button button-primary" value="Push Back Posts"></p>
+		</form>
+	</div><?php
+
+}
+
+add_action( 'admin_menu', function() {
+	add_submenu_page( 'tools.php', 'Better Scheduled Posts', 'Push Back Posts', 'edit_others_posts', 'bsp', 'bsp_tools_page' );
+} );
+
+// Clean permalinks for scheduled posts
+add_filter( 'post_link', function( $permalink, $post, $leavename ) {
+
+	if ( 'future' === $post->post_status ) {
+		$temp_post = clone $post;
+		$temp_post->post_status = 'publish';
+		$permalink = get_permalink( $temp_post );
+	}
+
+	return $permalink;
+}, 10, 3 );
+
+// Add scheduled posts to link box
+add_action( 'pre_get_posts', 'bsp_link_box' );
+function bsp_link_box( $query ) {
+
+	// Ensure request for internal linking
+	if ( ! isset( $_POST ) || ! isset( $_POST[ 'action' ] ) || 'wp-link-ajax' !== $_POST[ 'action' ] )
+		return;
+
+	// Add future posts to the query
+	$post_status = (array) $query->query_vars[ 'post_status' ];
+	if ( !in_array( 'future', $post_status ) )
+		$post_status[] = 'future';
+
+	$query->set( 'post_status', $post_status );
+}
---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== Better Scheduled Posts ===
+Contributors: carlomanf
+Tags: schedule
+Tested up to: 4.2
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Improves the management of your scheduled Posts.
+
+== Description ==
+
+Better Scheduled Posts improves the management of your scheduled Posts by:
+
+- making them visible on the front end to administrators/contributors
+- adding them to the internal link box
+- enabling you to push them back by any number of days.
+
+= Front End Visibility =
+
+Does it frustrate you when you schedule a post to publish and you can't see it when you view your website?
+
+Install this plugin and your scheduled Posts will become visible on the front end as if they were already published, but only to administrators/contributors. Your audience doesn't see the posts until the date/time they are scheduled to publish.
+
+= Internal Links =
+
+Does it frustrate you when you're writing a Post and you want to link to another Post that's scheduled but not published yet, and it doesn't show up in the Insert Link box?
+
+Install this plugin and you'll be able to easily add links to scheduled Posts the same way that you add links to published Posts.
+
+= Push Back Posts =
+
+Have you ever scheduled a number of posts to publish, perhaps on successive days, and your plans change and you need to push them all back a day (or number of days)?
+
+Install this plugin and you'll be able to push back all your scheduled Posts by any number of days, all at once instead of one by one.
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release
